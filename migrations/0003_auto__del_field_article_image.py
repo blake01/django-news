1e356811cd249d87358b924c40444028c82ce779
Blake Hemingway <blakehemingway@gmail.com>
2014-12-14 19:16:31 +0000

    Revert "Rebase migrations"
    This reverts commit 548c34e8b070119c9ff29209f620b22f9814f04f.

--- migrations/0003_auto__del_field_article_image.py
+++ migrations/0003_auto__del_field_article_image.py
@@ -8,21 +8,19 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Adding model 'Article'
-        db.create_table(u'news_article', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=128)),
-            ('slug', self.gf('autoslug.fields.AutoSlugField')(unique_with=(), max_length=50, populate_from='title')),
-            ('date', self.gf('django.db.models.fields.DateField')()),
-            ('content', self.gf('django.db.models.fields.TextField')()),
-            ('live', self.gf('django.db.models.fields.BooleanField')(default=True)),
-        ))
-        db.send_create_signal(u'news', ['Article'])
+        # Deleting field 'Article.image'
+        db.delete_column(u'news_article', 'image_id')
 
 
     def backwards(self, orm):
-        # Deleting model 'Article'
-        db.delete_table(u'news_article')
+
+        # User chose to not deal with backwards NULL issues for 'Article.image'
+        raise RuntimeError("Cannot reverse this migration. 'Article.image' and its values cannot be restored.")
+        
+        # The following code is provided here to aid in writing a correct migration        # Adding field 'Article.image'
+        db.add_column(u'news_article', 'image',
+                      self.gf('django.db.models.fields.related.ForeignKey')(to=orm['society.LibraryImage']),
+                      keep_default=False)
 
 
     models = {
